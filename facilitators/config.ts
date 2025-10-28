21057eacf652400544945c8173d4e128a40be8fd
basedx402 <basedx402@gmail.com>
2025-10-28 12:55:00 -0700

    rem polygon for now - still tseting

--- facilitators/config.ts
+++ facilitators/config.ts
@@ -210,14 +210,6 @@ const _FACILITATORS = validateUniqueFacilitators([
           enabled: true,
         },
       ],
-      [Chain.POLYGON]: [
-        {
-          address: '0x97316fa4730bc7d3b295234f8e4d04a0a4c093e8',
-          token: USDC_POLYGON_TOKEN,
-          syncStartDate: new Date('2025-10-16'),
-          enabled: false,
-        },
-      ],
     },
   },
 ] as const);
