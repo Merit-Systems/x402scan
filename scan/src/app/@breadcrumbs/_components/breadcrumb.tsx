fc0b4e9c56372d9b24beb21267d3f81d08246742
sragss <sammage@gmail.com>
2025-10-28 16:11:03 -0400

    don't cutoff lucide wallet

--- scan/src/app/@breadcrumbs/_components/breadcrumb.tsx
+++ scan/src/app/@breadcrumbs/_components/breadcrumb.tsx
@@ -45,7 +45,7 @@ export const Breadcrumb = <T extends string>({
         {(Fallback !== null || image !== null) && (
           <Avatar
             className={cn(
-              'rounded-md overflow-hidden bg-card size-5',
+              'rounded-md overflow-hidden bg-card size-5 flex-shrink-0',
               mobileHideImage && 'hidden md:block'
             )}
           >
@@ -67,7 +67,7 @@ export const Breadcrumb = <T extends string>({
 
         <p
           className={cn(
-            'font-semibold text-sm font-mono md:text-base truncate max-w-full',
+            'font-semibold text-sm font-mono md:text-base truncate min-w-0',
             textClassName,
             mobileHideText && 'hidden md:block'
           )}
