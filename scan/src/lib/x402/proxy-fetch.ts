f32513975edb70341e8dcdfab79bfd1d627d63f8
Alvaro Echevarria Cuesta <alvaroecuesta@gmail.com>
2025-10-28 13:10:57 -0400

    rm changes to scan. We will do that in a separate PR

--- scan/src/lib/x402/proxy-fetch.ts
+++ scan/src/lib/x402/proxy-fetch.ts
@@ -12,9 +12,7 @@ export const fetchWithProxy = async (
   } else {
     url = input.toString();
   }
-  console.log('env.NEXT_PUBLIC_PROXY_URL', env.NEXT_PUBLIC_PROXY_URL);
-  const proxyUrl = new URL(PROXY_ENDPOINT, env.NEXT_PUBLIC_PROXY_URL);
-  console.log('proxyUrl', proxyUrl);
+  const proxyUrl = new URL(PROXY_ENDPOINT, env.NEXT_PUBLIC_APP_URL);
   proxyUrl.searchParams.set('url', encodeURIComponent(url));
   proxyUrl.searchParams.set('share_data', 'true');
 
