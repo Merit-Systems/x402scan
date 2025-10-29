dce1ff3d6c33d6d116de19f4b7df7f8eb04be630
Jason <jason@hedmans.org>
2025-10-29 14:29:11 -0400

    add facilitators to workspace

--- facilitators/src/index.ts
+++ facilitators/src/index.ts
@@ -1,5 +1,15 @@
 // Configuration and individual facilitators
 export {
+  coinbase,
+  aurracloud,
+  thirdweb,
+  x402rs,
+  payai,
+  corbits,
+  dexter,
+  daydreams,
+  mogami,
+  openx402,
   FACILITATORS,
   FACILITATORS_BY_CHAIN,
   BASE_FACILITATORS,
