575c0519e0e6816a40d7e0496b836c7591b74cbd
shafu <shafu0x@gmail.com>
2025-10-30 16:07:41 -0400

    do in parallel

--- sync-analytics/trigger/queries/uptimes/persist.ts
+++ sync-analytics/trigger/queries/uptimes/persist.ts
@@ -10,27 +10,29 @@ export async function persistUptimes(data: unknown) {
 
       logger.log('Fetched uptimes from analytics', { count: uptimes.length });
 
-      for (const uptime of uptimes) {
-        await db.uptime.upsert({
-          where: { url: uptime.url },
-          update: {
-            totalCount24h: parseInt(uptime.total_count_24h.toString()),
-            uptime1hPct: uptime.uptime_1h_pct,
-            uptime6hPct: uptime.uptime_6h_pct,
-            uptime24hPct: uptime.uptime_24h_pct,
-            uptimeAllTimePct: uptime.uptime_all_time_pct,
-            updatedAt: new Date(),
-          },
-          create: {
-            url: uptime.url,
-            totalCount24h: parseInt(uptime.total_count_24h.toString()),
-            uptime1hPct: uptime.uptime_1h_pct,
-            uptime6hPct: uptime.uptime_6h_pct,
-            uptime24hPct: uptime.uptime_24h_pct,
-            uptimeAllTimePct: uptime.uptime_all_time_pct,
-          },
-        });
-      }
+      await Promise.all(
+        uptimes.map(uptime =>
+          db.uptime.upsert({
+            where: { url: uptime.url },
+            update: {
+              totalCount24h: parseInt(uptime.total_count_24h.toString()),
+              uptime1hPct: uptime.uptime_1h_pct,
+              uptime6hPct: uptime.uptime_6h_pct,
+              uptime24hPct: uptime.uptime_24h_pct,
+              uptimeAllTimePct: uptime.uptime_all_time_pct,
+              updatedAt: new Date(),
+            },
+            create: {
+              url: uptime.url,
+              totalCount24h: parseInt(uptime.total_count_24h.toString()),
+              uptime1hPct: uptime.uptime_1h_pct,
+              uptime6hPct: uptime.uptime_6h_pct,
+              uptime24hPct: uptime.uptime_24h_pct,
+              uptimeAllTimePct: uptime.uptime_all_time_pct,
+            },
+          })
+        )
+      );
     } catch (error) {
       logger.error('Error in uptimes sync task:', {
         error: String(error),
