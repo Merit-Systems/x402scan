740decb26c6a8d792588c43177740539c45ce854
shafu <shafu0x@gmail.com>
2025-10-31 15:28:54 -0400

    rm logs

--- sync/analytics/trigger/queries/by_domain/persist.ts
+++ sync/analytics/trigger/queries/by_domain/persist.ts
@@ -5,24 +5,16 @@ import { mapMetric } from '../utils';
 
 export async function persistMetrics(data: unknown) {
   try {
-    logger.log('Persisting uptimes', { data });
-
     const metrics: MetricsByDomain[] = data as MetricsByDomain[];
 
     logger.log('Fetched metrics from analytics', { count: metrics.length });
 
-    for (const metric of metrics) {
-      await db.resourceDomainMetrics.upsert({
-        where: { domain: metric.domain },
-        create: {
-          domain: metric.domain,
-          ...mapMetric(metric),
-        },
-        update: {
-          ...mapMetric(metric),
-        },
-      });
-    }
+    await db.resourceDomainMetrics.createMany({
+      data: metrics.map(metric => ({
+        domain: metric.domain,
+        ...mapMetric(metric),
+      })),
+    });
 
     } catch (error) {
       logger.error('Error in uptimes sync task:', {
--- sync/analytics/trigger/queries/by_url/persist.ts
+++ sync/analytics/trigger/queries/by_url/persist.ts
@@ -5,24 +5,16 @@ import { mapMetric } from "../utils";
 
 export async function persistMetrics(data: unknown) {
   try {
-    logger.log('Persisting metrics by url', { data });
-
     const metrics: MetricsByUrl[] = data as MetricsByUrl[];
 
     logger.log('Fetched metrics by url', { count: metrics.length });
 
-    for (const metric of metrics) {
-      await db.resourceUrlMetrics.upsert({
-        where: { url: metric.url },
-        create: {
-          url: metric.url,
-          ...mapMetric(metric),
-        },
-        update: {
-          ...mapMetric(metric),
-        },
-      });
-    }
+    await db.resourceUrlMetrics.createMany({
+      data: metrics.map(metric => ({
+        url: metric.url,
+        ...mapMetric(metric),
+      })),
+    });
 
     } catch (error) {
       logger.error('Error in metrics by url sync task:', {
