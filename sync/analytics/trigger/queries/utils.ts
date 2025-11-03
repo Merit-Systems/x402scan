0c26548740fb8b97534ea4427b16ba3821bf90d3
shafu <shafu0x@gmail.com>
2025-11-03 15:35:19 -0500

    format

--- sync/analytics/trigger/queries/utils.ts
+++ sync/analytics/trigger/queries/utils.ts
@@ -1,8 +1,6 @@
 import { Metrics } from './types';
 
-function toNumber(
-  value: number | string | null | undefined
-): number | null {
+function toNumber(value: number | string | null | undefined): number | null {
   if (value === null || value === undefined) {
     return null;
   }
