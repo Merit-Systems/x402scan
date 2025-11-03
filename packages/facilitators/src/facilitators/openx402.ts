ccd2cae3bd88b6b4f2b6740235f6ff4e4e3febd2
basedx402 <basedx402@gmail.com>
2025-11-02 21:36:01 -0800

    Polygon and 1 more facilitator

--- packages/facilitators/src/facilitators/openx402.ts
+++ packages/facilitators/src/facilitators/openx402.ts
@@ -1,10 +1,10 @@
 import { Network } from '../types';
-import { USDC_BASE_TOKEN, USDC_SOLANA_TOKEN } from '../constants';
+import { USDC_BASE_TOKEN, USDC_SOLANA_TOKEN, USDC_POLYGON_TOKEN } from '../constants';
 
 import type { Facilitator, FacilitatorConfig } from '../types';
 
 export const openx402: FacilitatorConfig = {
-  url: 'https://open.x402.host',
+  url: 'https://openx402.ai',
 };
 
 export const openx402Facilitator = {
@@ -12,7 +12,7 @@ export const openx402Facilitator = {
   metadata: {
     name: 'OpenX402',
     image: 'https://x402scan.com/openx402.png',
-    docsUrl: 'https://open.x402.host',
+    docsUrl: 'https://openx402.ai',
     color: '#E8EFFE',
   },
   config: openx402,
@@ -28,12 +28,34 @@ export const openx402Facilitator = {
         tokens: [USDC_BASE_TOKEN],
         dateOfFirstTransaction: new Date('2025-10-16'),
       },
+      {
+        address: '0x241b91ba395da56B3b6cc816138C20db6c6B37D3',
+        tokens: [USDC_BASE_TOKEN],
+        dateOfFirstTransaction: new Date('2025-11-11'),
+      },
     ],
     [Network.SOLANA]: [
       {
         address: '5xvht4fYDs99yprfm4UeuHSLxMBRpotfBtUCQqM3oDNG',
         tokens: [USDC_SOLANA_TOKEN],
         dateOfFirstTransaction: new Date('2025-10-16'),
+      },
+    ],
+    [Network.POLYGON]: [
+      {
+        address: '0x97316fa4730bc7d3b295234f8e4d04a0a4c093e8',
+        tokens: [USDC_POLYGON_TOKEN],
+        dateOfFirstTransaction: new Date('2025-11-01'),
+      },
+      {
+        address: '0x97db9b5291a218fc77198c285cefdc943ef74917',
+        tokens: [USDC_POLYGON_TOKEN],
+        dateOfFirstTransaction: new Date('2025-11-01'),
+      },
+      {
+        address: '0x241b91ba395da56B3b6cc816138C20db6c6B37D3',
+        tokens: [USDC_POLYGON_TOKEN],
+        dateOfFirstTransaction: new Date('2025-11-01'),
       },
     ],
   },
