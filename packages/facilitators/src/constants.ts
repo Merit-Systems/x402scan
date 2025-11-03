57ee7e4d53db51d03c8b1b7e5e51f7e0445d39e7
BONG <241839524+bongtoken@users.noreply.github.com>
2025-11-03 23:43:57 +0300

    Update constants.ts

--- packages/facilitators/src/constants.ts
+++ packages/facilitators/src/constants.ts
@@ -1,9 +1,31 @@
 import type { Token } from './types';
 
 const USDC_DECIMALS = 6;
+const DECIMALS_ON_BNB = 18;
 const USDC_SOLANA = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v';
 const USDC_BASE = '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913'; // USDC on Base
 const USDC_POLYGON = '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359'; // USDC on Polygon
+const USDT_BNB = '0x55d398326f99059fF775485246999027B3197955'; // USDT on BNB
+const USD1_BNB = '0x8d0D000Ee44948FC98c9B98A4FA4921476f08B0d'; // USD1 on BNB
+const USDC_BNB = '0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d'; // USDC on BNB
+
+export const USDT_BNB_TOKEN: Token = {
+  address: USDT_BNB,
+  decimals: DECIMALS_ON_BNB,
+  symbol: 'USDT',
+};
+
+export const USDC_BNB_TOKEN: Token = {
+  address: USD1_BNB,
+  decimals: DECIMALS_ON_BNB,
+  symbol: 'USD1',
+};
+
+export const USDC_BNB_TOKEN: Token = {
+  address: USDC_BNB,
+  decimals: DECIMALS_ON_BNB,
+  symbol: 'USDC',
+};
 
 export const USDC_BASE_TOKEN: Token = {
   address: USDC_BASE,
