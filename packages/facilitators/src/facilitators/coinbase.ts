92ecef34b107e0b3617ce2629daa0691688ceb17
erik <erik.reppel@coinbase.com>
2025-11-02 18:01:47 -0800

    Add new CB facilitator signers

--- packages/facilitators/src/facilitators/coinbase.ts
+++ packages/facilitators/src/facilitators/coinbase.ts
@@ -25,6 +25,51 @@ export const coinbaseFacilitator = {
         tokens: [USDC_BASE_TOKEN],
         dateOfFirstTransaction: new Date('2025-05-05'),
       },
+      {
+        address: '0x9aAE2B0d1b9dC55Ac9Bab9556F9a26Cb64995Fb9'.toLowerCase(),
+        tokens: [USDC_BASE_TOKEN],
+        dateOfFirstTransaction: new Date('2025-10-31'),
+      },
+      {
+        address: '0x3A70788150c7645a21b95b7062ab1784D3cc2104'.toLowerCase(),
+        tokens: [USDC_BASE_TOKEN],
+        dateOfFirstTransaction: new Date('2025-10-31'),
+      },
+      {
+        address: '0x708E57B6650a9a741Ab39CAE1969Ea1D2D10ECA1'.toLowerCase(),
+        tokens: [USDC_BASE_TOKEN],
+        dateOfFirstTransaction: new Date('2025-10-31'),
+      },
+      {
+        address: '0xCE82Eeec8e98e443EC34Fda3c3E999Cbe4Cb6aC2'.toLowerCase(),
+        tokens: [USDC_BASE_TOKEN],
+        dateOfFirstTransaction: new Date('2025-10-31'),
+      },
+      {
+        address: '0x7f6D822467Df2A85F792d4508c5722ade96Be056'.toLowerCase(),
+        tokens: [USDC_BASE_TOKEN],
+        dateOfFirstTransaction: new Date('2025-10-31'),
+      },
+      {
+        address: '0x001dDaBbA5782eE48842318BD9FF4008647c8D9C'.toLowerCase(),
+        tokens: [USDC_BASE_TOKEN],
+        dateOfFirstTransaction: new Date('2025-10-31'),
+      },
+      {
+        address: '0x9C09FAa49C4235A09677159fF14f17498ac48738'.toLowerCase(),
+        tokens: [USDC_BASE_TOKEN],
+        dateOfFirstTransaction: new Date('2025-10-31'),
+      },
+      {
+        address: '0xcbB10C30a9a72fae9232F41CBbd566a097B4E03a'.toLowerCase(),
+        tokens: [USDC_BASE_TOKEN],
+        dateOfFirstTransaction: new Date('2025-10-31'),
+      },
+      {
+        address: '0x9fb2714AF0A84816F5C6322884F2907E33946b88'.toLowerCase(),
+        tokens: [USDC_BASE_TOKEN],
+        dateOfFirstTransaction: new Date('2025-10-31'),
+      },
     ],
     [Network.SOLANA]: [
       {
