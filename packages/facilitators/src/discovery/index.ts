e4e1ff8f20c17485caaedb3c0f8613ea595605a3
An <anqtran36@gmail.com>
2025-11-04 16:17:08 -0500

    address comments

--- packages/facilitators/src/discovery/index.ts
+++ packages/facilitators/src/discovery/index.ts
@@ -1,13 +1,14 @@
 export {
-  listFacilitatorResources,
   listAllFacilitatorResources,
+  listFacilitatorResources,
 } from './list-resources';
 
+export { anyspendDiscovery } from '../facilitators/anyspend';
+export { aurracloudDiscovery } from '../facilitators/aurracloud';
 export { coinbaseDiscovery } from '../facilitators/coinbase';
 export { payaiDiscovery } from '../facilitators/payai';
-export { aurracloudDiscovery } from '../facilitators/aurracloud';
-export { thirdwebDiscovery } from '../facilitators/thirdweb';
 export { questflowDiscovery } from '../facilitators/questflow';
+export { thirdwebDiscovery } from '../facilitators/thirdweb';
 export { ultravioletadaoDiscovery } from '../facilitators/ultravioletadao';
 
 export { discoverableFacilitators } from './facilitators';
