1da82b0fa34b36c4c0064aa21dc655d1b9f93a88
Jason <jason@hedmans.org>
2025-11-12 15:21:21 -0500

    update scan db package

--- databases/scan/src/client.ts
+++ databases/scan/src/client.ts
@@ -1,8 +1,26 @@
-import { PrismaClient } from '../generated/client';
+import { PrismaClient } from '@prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
 
-const globalForPrisma = global as unknown as { scanDb: PrismaClient };
+import { neonConfig } from '@neondatabase/serverless';
 
-export const scanDb = globalForPrisma.scanDb || new PrismaClient();
+import ws from 'ws';
+
+neonConfig.webSocketConstructor = ws;
+
+const connectionString = `${process.env.POSTGRES_PRISMA_URL}`;
+
+const globalForPrisma = global as unknown as {
+  scanDb: PrismaClient;
+  scanDbAdapter: PrismaNeon;
+};
+
+const scanDbAdapter =
+  globalForPrisma.scanDbAdapter || new PrismaNeon({ connectionString });
+if (process.env.NODE_ENV !== 'production')
+  globalForPrisma.scanDbAdapter = scanDbAdapter;
+
+export const scanDb =
+  globalForPrisma.scanDb || new PrismaClient({ adapter: scanDbAdapter });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.scanDb = scanDb;
 
