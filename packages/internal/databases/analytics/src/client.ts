dd847af1d5963f3443dcf133dd6628e7fd7909cd
Jason <jason@hedmans.org>
2025-11-13 17:43:38 -0500

    update analytics db client creation

--- packages/internal/databases/analytics/src/client.ts
+++ packages/internal/databases/analytics/src/client.ts
@@ -1,11 +1,14 @@
 import { createClient } from '@clickhouse/client';
 
-export const analyticsDb = createClient({
-  url: process.env.ANALYTICS_CLICKHOUSE_URL ?? 'http://localhost:8123',
-  username: process.env.ANALYTICS_CLICKHOUSE_USER ?? 'default',
-  password: process.env.ANALYTICS_CLICKHOUSE_PASSWORD ?? '',
-  database: process.env.ANALYTICS_CLICKHOUSE_DATABASE ?? 'default',
-  request_timeout: Number(
-    process.env.ANALYTICS_CLICKHOUSE_REQUEST_TIMEOUT ?? 10000
-  ),
-});
+export const createAnalyticsDb = () =>
+  createClient({
+    url: process.env.ANALYTICS_CLICKHOUSE_URL ?? 'http://localhost:8123',
+    username: process.env.ANALYTICS_CLICKHOUSE_USER ?? 'default',
+    password: process.env.ANALYTICS_CLICKHOUSE_PASSWORD ?? '',
+    database: process.env.ANALYTICS_CLICKHOUSE_DATABASE ?? 'default',
+    request_timeout: Number(
+      process.env.ANALYTICS_CLICKHOUSE_REQUEST_TIMEOUT ?? 10000
+    ),
+  });
+
+export const analyticsDb = createAnalyticsDb();
--- packages/internal/databases/analytics/src/utils.ts
+++ packages/internal/databases/analytics/src/utils.ts
@@ -1,16 +1,16 @@
-import { analyticsDb } from './client';
+import { createAnalyticsDb } from './client';
 import type { Tables } from './tables/types';
 import type { AnalyticsDatabaseMap } from './tables/types';
 
 export const createTable = async (query: string) => {
-  return analyticsDb.exec({ query });
+  return createAnalyticsDb().exec({ query });
 };
 
 export const insertData = async <T extends Tables>(
   table: Tables,
   data: AnalyticsDatabaseMap[T][]
 ) => {
-  return analyticsDb.insert<AnalyticsDatabaseMap[T]>({
+  return createAnalyticsDb().insert<AnalyticsDatabaseMap[T]>({
     table,
     values: data,
     format: 'JSONEachRow',
