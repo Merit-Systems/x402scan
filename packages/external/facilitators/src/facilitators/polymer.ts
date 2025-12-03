a2c76ea154141187e59e3989e6b2e9f1b2ae03fe
oblackmon3 <odell@polymerlabs.org>
2025-12-03 12:08:47 -0500

    fix : Update faciliator address

--- packages/external/facilitators/src/facilitators/polymer.ts
+++ packages/external/facilitators/src/facilitators/polymer.ts
@@ -19,7 +19,7 @@ export const polymerFacilitator = {
   addresses: {
     [Network.BASE]: [
       {
-        address: '0xd9bbe98d78c1309e61c3cd50cd9329dcc29e99df',
+        address: '0xc4f6017a2345db17d5a6eb4de7d6c63f0d55c9ac',
         tokens: [USDC_BASE_TOKEN],
         dateOfFirstTransaction: new Date('2025-10-27'),
       },
