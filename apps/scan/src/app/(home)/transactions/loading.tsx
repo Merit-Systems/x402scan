ec8d0cfa8e1da860dfc32710c71f273d10e1e0a1
Mason Hall <masonhall@gmail.com>
2026-01-06 16:03:15 -0500

    fix build error

--- apps/scan/src/app/(home)/transactions/loading.tsx
+++ apps/scan/src/app/(home)/transactions/loading.tsx
@@ -3,6 +3,7 @@ import { Body, Heading } from '@/app/_components/layout/page-utils';
 import { LoadingLatestTransactionsTable } from '../_components/transactions';
 
 export default function LoadingTransactionsPage() {
+  const pageSize = 10;
   return (
     <div>
       <Heading
@@ -10,7 +11,7 @@ export default function LoadingTransactionsPage() {
         description="All x402 transactions through facilitators we track"
       />
       <Body>
-        <LoadingLatestTransactionsTable loadingRowCount={15} />
+        <LoadingLatestTransactionsTable loadingRowCount={pageSize} />
       </Body>
     </div>
   );
