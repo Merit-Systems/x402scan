2277624801e343d9df3e3088153ac5da4f796879
Mason Hall <masonhall@gmail.com>
2026-01-07 16:37:58 -0500

    suspense

--- apps/scan/src/app/composer/(home)/page.tsx
+++ apps/scan/src/app/composer/(home)/page.tsx
@@ -1,5 +1,7 @@
+import { Suspense } from 'react';
+
 import { Body } from '@/app/_components/layout/page-utils';
-import { Agents } from './_components/agents';
+import { Agents, LoadingAgents } from './_components/agents';
 import { ComposerHomeHeading } from './_components/heading';
 import { Tools } from './_components/tools';
 import { OverallStats } from './_components/stats';
@@ -14,8 +16,14 @@ export default async function ComposerPage() {
     <div>
       <ComposerHomeHeading />
       <Body>
-        {session?.user?.id && <YourAgents userId={session.user.id} />}
-        <Agents />
+        {session?.user?.id && (
+          <Suspense fallback={<LoadingAgents />}>
+            <YourAgents userId={session.user.id} />
+          </Suspense>
+        )}
+        <Suspense fallback={<LoadingAgents />}>
+          <Agents />
+        </Suspense>
         <Tools />
         <Feed />
         <OverallStats />
