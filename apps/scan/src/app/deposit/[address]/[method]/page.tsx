68b45cbd1372d6869a7a61c8f9ac7136412d9c35
Jason Hedman <40368124+jasonhedman@users.noreply.github.com>
2026-01-13 14:28:58 -0500

    types

--- apps/scan/src/app/deposit/[address]/[method]/page.tsx
+++ apps/scan/src/app/deposit/[address]/[method]/page.tsx
@@ -1,10 +1,16 @@
-import { OnrampMethods, OnrampProviders } from '@/services/onramp/types';
 import z from 'zod';
+
 import { notFound } from 'next/navigation';
+
 import { METHODS } from './_components/methods';
 import { Body } from '@/app/_components/layout/page-utils';
 
+import { ethereumAddressSchema } from '@/lib/schemas';
+
+import { OnrampMethods, OnrampProviders } from '@/services/onramp/types';
+
 const paramsSchema = z.object({
+  address: ethereumAddressSchema,
   method: z.union([z.literal(OnrampMethods.WALLET), z.enum(OnrampProviders)]),
 });
 
@@ -18,13 +24,13 @@ export default async function SuccessPage({
     return notFound();
   }
 
-  const { method } = parsedParams.data;
+  const { method, address } = parsedParams.data;
 
   const Component = METHODS[method];
 
   return (
     <Body className="max-w-lg mx-auto">
-      <Component searchParams={await searchParams} />
+      <Component searchParams={await searchParams} address={address} />
     </Body>
   );
 }
