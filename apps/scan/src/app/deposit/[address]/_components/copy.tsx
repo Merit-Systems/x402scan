b764c7d485e65c8e944b085d07b6dd5069578ab1
Jason Hedman <40368124+jasonhedman@users.noreply.github.com>
2026-01-13 10:09:16 -0500

    wallet copy button

---
+++ apps/scan/src/app/deposit/[address]/_components/copy.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import { useCopyToClipboard } from '@/hooks/use-copy-to-clipboard';
+import { Check, Wallet } from 'lucide-react';
+import { toast } from 'sonner';
+
+import type { Address } from 'viem';
+
+interface Props {
+  address: Address;
+}
+
+export const CopyAddress: React.FC<Props> = ({ address }) => {
+  const { isCopied, copyToClipboard } = useCopyToClipboard(() => {
+    toast.success('Address copied to clipboard');
+  });
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button
+          onClick={() => void copyToClipboard(address)}
+          variant="outline"
+          className="shrink-0 size-fit md:size-fit p-2"
+          size="icon"
+        >
+          {isCopied ? (
+            <Check className="size-3" />
+          ) : (
+            <Wallet className="size-3" />
+          )}
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent side="bottom">
+        <p className="font-mono">{address}</p>
+      </TooltipContent>
+    </Tooltip>
+  );
+};
--- apps/scan/src/app/deposit/[address]/page.tsx
+++ apps/scan/src/app/deposit/[address]/page.tsx
@@ -1,9 +1,13 @@
-import { ethereumAddressSchema } from '@/lib/schemas';
+import { Body } from '@/app/_components/layout/page-utils';
+
+import { CopyAddress } from './_components/copy';
 import { Deposit } from './_components';
+
 import { WalletChainProvider } from '@/app/_contexts/wallet-chain/provider';
+
+import { ethereumAddressSchema } from '@/lib/schemas';
+
 import { Chain } from '@/types/chain';
-import { Body, Heading } from '@/app/_components/layout/page-utils';
-import { CopyCode } from '@/components/ui/copy-code';
 
 export default async function DepositPage({
   params,
@@ -22,17 +26,15 @@ export default async function DepositPage({
       <div className="flex flex-col flex-1 py-6 md:py-8">
         <WalletChainProvider initialChain={Chain.BASE} isFixed>
           <Body className="max-w-lg mx-auto">
-            <div className="flex flex-col items-center gap-1">
-              <h1 className="text-3xl font-bold font-mono">Add Funds</h1>
-              <p className="text-muted-foreground">
-                Add USDC to your x402scan MCP to call resources
-              </p>
+            <div className="flex items-center gap-2 justify-between">
+              <div className="flex flex-col items-start gap-1">
+                <h1 className="text-3xl font-bold font-mono">Add Funds</h1>
+                <p className="text-muted-foreground">
+                  Add USDC to your x402scan MCP to call resources
+                </p>
+              </div>
+              <CopyAddress address={parsedAddress.data} />
             </div>
-            <CopyCode
-              code={parsedAddress.data}
-              toastMessage="Address copied to clipboard"
-            />
-
             <Deposit address={parsedAddress.data} />
           </Body>
         </WalletChainProvider>
