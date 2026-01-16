09825cdc536f94ece8b6c6b391cc003ab919cfe9
Jason Hedman <40368124+jasonhedman@users.noreply.github.com>
2026-01-16 18:59:25 -0500

    update page layout

--- apps/scan/src/app/mcp/page.tsx
+++ apps/scan/src/app/mcp/page.tsx
@@ -7,28 +7,28 @@ import { Badge } from '@/components/ui/badge';
 
 export default function McpPage() {
   return (
-    <Body className="max-w-lg mx-auto">
-      <div className="flex flex-col gap-4 items-center justify-center text-center">
-        <Badge className="bg-primary/10 text-primary border-primary/60 hover:bg-primary/20">
-          Now Available
-        </Badge>
-        <h1 className="text-5xl font-bold font-mono">x402scan MCP</h1>
-        <p>
-          Make paid API requests to any x402 endpoint from Claude Code, Cursor,
-          and more.
-        </p>
+    <Body className="max-w-lg mx-auto gap-16">
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-4 items-center justify-center text-center">
+          <Badge className="bg-primary/10 text-primary border-primary/60 hover:bg-primary/20">
+            Now Available
+          </Badge>
+          <h1 className="text-5xl font-bold">x402scan MCP</h1>
+          <p className="font-mono text-sm">
+            Make paid API requests to any x402 endpoint from Claude Code,
+            Cursor, and more.
+          </p>
+        </div>
+        <div className="flex justify-center">
+          <CopyCode
+            code={'npx @x402scan/mcp install'}
+            toastMessage="MCP Server URL copied to clipboard"
+            className="bg-transparent w-fit pl-3 font-bold text-primary"
+            copyButtonClassName="bg-transparent shadow-none border-0"
+          />
+        </div>
+        <HeroGraphic />
       </div>
-      <HeroGraphic />
-      <CopyCode
-        code={'npx -y @x402scan/mcp install'}
-        toastMessage="MCP Server URL copied to clipboard"
-        className="bg-primary/10"
-        copyButtonClassName="bg-transparent hover:bg-primary/20 shadow-none border-0"
-      />
-      <TextSeparator
-        text="or install on your client"
-        textClassName="text-muted-foreground/50"
-      />
       <Clients />
     </Body>
   );
