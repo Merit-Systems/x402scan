026727e7c8b28fecdbcfde4fdc103fd1073cb9a1
Jason Hedman <40368124+jasonhedman@users.noreply.github.com>
2026-01-16 21:01:28 -0500

    hero graphic

--- apps/scan/src/app/mcp/_components/graphic/graphic.tsx
+++ apps/scan/src/app/mcp/_components/graphic/graphic.tsx
@@ -31,13 +31,18 @@ export const HeroGraphic = () => {
     0, 2,
   ]);
 
-  const cardClassName = (isActive: boolean) =>
-    cn(
+  const cardProps = (isActive: boolean, side: 'left' | 'right') => ({
+    className: cn(
       'flex flex-col items-center justify-center p-2 z-10 bg-card relative transition-all duration-500 border-2',
       isActive
-        ? 'border-primary/80 shadow-primary/60 shadow-[0_0_8px]'
-        : 'opacity-50'
-    );
+        ? `border-primary/80 shadow-primary/60 shadow-[0_0_8px]`
+        : 'opacity-50',
+      isActive &&
+        ((side === 'left' && !isReverse) ||
+          (side === 'right' && isReverse && !isPending)) &&
+        'animate-pulse-active'
+    ),
+  });
   const imageSize = 32;
   const imageProps = {
     width: imageSize,
@@ -84,7 +89,7 @@ export const HeroGraphic = () => {
 
   useEffect(() => {
     let interval: NodeJS.Timeout;
-    const offset = (beamDuration / 4) * 1000;
+    const offset = (beamDuration / 8) * 1000;
     const intervalTime = beamDuration * 2 * 1000;
 
     // Initial timeout to offset the interval by duration/2
@@ -106,6 +111,7 @@ export const HeroGraphic = () => {
     reverse: isReverse,
     beamWidth: isActive && !isPending ? 10 : 0,
     pathOpacity: isActive ? 0.5 : 0.05,
+    delay: 0.5,
   });
 
   const echoEndClassName = 'absolute h-full w-0 top-1/2 -translate-y-1/2';
@@ -116,7 +122,7 @@ export const HeroGraphic = () => {
       className="relative size-full flex items-center justify-between max-w-lg w-full"
     >
       <div className="flex flex-col gap-8">
-        <Card className={cardClassName(activeClientIndex === 0)}>
+        <Card {...cardProps(activeClientIndex === 0, 'left')}>
           <ClientIcon
             client={Clients.ClaudeCode}
             {...imageProps}
@@ -130,7 +136,7 @@ export const HeroGraphic = () => {
             ref={client1Ref}
           />
         </Card>
-        <Card className={cardClassName(activeClientIndex === 1)}>
+        <Card {...cardProps(activeClientIndex === 1, 'left')}>
           <ClientIcon
             client={Clients.Cursor}
             {...imageProps}
@@ -144,7 +150,7 @@ export const HeroGraphic = () => {
             ref={client2Ref}
           />
         </Card>
-        <Card className={cardClassName(activeClientIndex === 2)}>
+        <Card {...cardProps(activeClientIndex === 2, 'left')}>
           <ClientIcon
             client={Clients.Codex}
             {...imageProps}
@@ -164,22 +170,46 @@ export const HeroGraphic = () => {
         <div className={cn(echoEndClassName, 'right-10')} ref={echoRightRef} />
       </Chip>
       <div className="flex flex-col gap-8">
-        <Card className={cardClassName(activeServerIndices.includes(0))}>
-          <ImageIcon {...imageProps} />
+        <Card {...cardProps(activeServerIndices.includes(0), 'right')}>
+          <ImageIcon
+            {...imageProps}
+            className={cn(
+              imageProps.className,
+              activeServerIndices.includes(0)
+                ? 'stroke-primary'
+                : 'stroke-current'
+            )}
+          />
           <div
             className={targetLeftClassName(activeServerIndices.includes(0))}
             ref={server1Ref}
           />
         </Card>
-        <Card className={cardClassName(activeServerIndices.includes(1))}>
-          <Search {...imageProps} />
+        <Card {...cardProps(activeServerIndices.includes(1), 'right')}>
+          <Search
+            {...imageProps}
+            className={cn(
+              imageProps.className,
+              activeServerIndices.includes(1)
+                ? 'stroke-primary'
+                : 'stroke-current'
+            )}
+          />
           <div
             className={targetLeftClassName(activeServerIndices.includes(1))}
             ref={server2Ref}
           />
         </Card>
-        <Card className={cardClassName(activeServerIndices.includes(2))}>
-          <ChartCandlestick {...imageProps} />
+        <Card {...cardProps(activeServerIndices.includes(2), 'right')}>
+          <ChartCandlestick
+            {...imageProps}
+            className={cn(
+              imageProps.className,
+              activeServerIndices.includes(2)
+                ? 'stroke-primary'
+                : 'stroke-current'
+            )}
+          />
           <div
             className={targetLeftClassName(activeServerIndices.includes(2))}
             ref={server3Ref}
