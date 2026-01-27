6999b71439e1a6b436ccd7e082dc814fb26176c3
Alvaro Echevarria Cuesta <alvaroecuesta@gmail.com>
2026-01-27 11:13:08 -0500

    update new schema

--- packages/internal/databases/partners/src/tables/partners.ts
+++ packages/internal/databases/partners/src/tables/partners.ts
@@ -1,17 +1,36 @@
 import type { InsertDataFunction } from './types';
 import { Tables } from './types';
-import { insertData } from '../utils';
+import { createTable, insertData } from '../utils';
+
+const partnersTable = `
+    CREATE TABLE IF NOT EXISTS ${Tables.Partners} (
+      id String,
+      name String,
+      email String,
+      organization String,
+      merit_contact String,
+      meeting_date Array(DateTime64(3)),
+      wallet_addresses Array(String),
+      invite_codes Array(String)
+    ) ENGINE = MergeTree()
+    ORDER BY (id)
+    SETTINGS index_granularity = 8192;
+`;
+
+export const createPartnersTable = async () => {
+  return createTable(partnersTable);
+};
 
 export const insertPartner: InsertDataFunction<Tables.Partners> = async data =>
   insertData(Tables.Partners, [data]);
 
 export interface PartnerData {
   id: string;
-  wallet_address: string | null;
-  invite_code: string | null;
-  name: string | null;
-  email: string | null;
-  organization: string | null;
-  merit_contact: string | null;
-  meeting_date: Date | null;
+  name: string;
+  email: string;
+  organization: string;
+  merit_contact: string;
+  meeting_date: Date[];
+  wallet_addresses: string[];
+  invite_codes: string[];
 }
