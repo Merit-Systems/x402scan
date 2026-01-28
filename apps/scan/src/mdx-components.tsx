34fc3660d1ce3096901e6df54bef49493ff3f7cc
Jason Hedman <40368124+jasonhedman@users.noreply.github.com>
2026-01-28 13:52:08 -0500

    add prompt template with parsing

--- apps/scan/src/mdx-components.tsx
+++ apps/scan/src/mdx-components.tsx
@@ -1,3 +1,7 @@
+import { isValidElement, type ReactElement } from 'react';
+
+import { PromptTemplate } from '@/components/prompt-template';
+
 import type { MDXComponents } from 'mdx/types';
 
 const components: MDXComponents = {
@@ -7,6 +11,39 @@ const components: MDXComponents = {
   h4: ({ children }) => <h4 className="text-base font-medium">{children}</h4>,
   h5: ({ children }) => <h5 className="text-sm font-medium">{children}</h5>,
   h6: ({ children }) => <h6 className="text-xs font-medium">{children}</h6>,
+  pre: ({ children, ...rest }) => {
+    if (
+      typeof children === 'object' &&
+      children !== null &&
+      isValidElement(children as unknown)
+    ) {
+      const childElement = children as ReactElement;
+      if (
+        typeof childElement.props === 'object' &&
+        childElement.props !== null &&
+        'className' in childElement.props &&
+        typeof childElement.props.className === 'string' &&
+        childElement.props.className.includes('prompt')
+      ) {
+        return childElement;
+      }
+    }
+    return <pre {...rest}>{children}</pre>;
+  },
+  code: ({ children, className, ...rest }) => {
+    if (
+      typeof className === 'string' &&
+      className.includes('prompt') &&
+      typeof children === 'string'
+    ) {
+      return <PromptTemplate templateString={children} />;
+    }
+    return (
+      <pre>
+        <code {...rest}>{children}</code>
+      </pre>
+    );
+  },
 };
 
 export function useMDXComponents(): MDXComponents {
