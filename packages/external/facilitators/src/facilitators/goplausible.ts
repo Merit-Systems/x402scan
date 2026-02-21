a09d1b88ff90c05417da2521d60a0ddcfa58c2a1
MG <emg110@gmail.com>
2026-02-21 18:38:18 +0300

    Fix first transaction dates

--- packages/external/facilitators/src/facilitators/goplausible.ts
+++ packages/external/facilitators/src/facilitators/goplausible.ts
@@ -24,23 +24,23 @@ export const goplausibleFacilitator = {
   addresses: {
     [Network.BASE]: [
       {
-        address: '0x136008978ad053942dCDBE759A0903f5d84966fa',
+        address: '0x136008978ad053942dcdbe759a0903f5d84966fa',
         tokens: [USDC_BASE_TOKEN],
-        dateOfFirstTransaction: new Date('2026-02-03'),
+        dateOfFirstTransaction: new Date('2026-02-18'),
       },
     ],
     [Network.SOLANA]: [
       {
         address: '8a8fFNfk2AGS7rgVv1BoqPUWnzQuoCrShJV8tSE6RAYi',
         tokens: [USDC_SOLANA_TOKEN],
-        dateOfFirstTransaction: new Date('2026-02-03'),
+        dateOfFirstTransaction: new Date('2026-02-18'),
       },
     ],
     [Network.ALGORAND]: [
       {
         address: 'ZMFK2OI7ZBD2U27ISERZC4S6LKM6WMFJPZQ4MYNJDZ2VNBNMBA67RA22AA',
         tokens: [USDC_ALGORAND_TOKEN],
-        dateOfFirstTransaction: new Date('2026-02-03'),
+        dateOfFirstTransaction: new Date('2026-02-13'),
       },
     ],
   },
