4ccf499055b9ef2bead02b229c2c3aedf5f32eac
shafu <shafu0x@gmail.com>
2026-03-06 10:45:27 -0500

    fix(scan): increase CAIP-2 chain reference max length to 64 chars

--- apps/scan/src/lib/x402/v2/schema.ts
+++ apps/scan/src/lib/x402/v2/schema.ts
@@ -4,7 +4,7 @@ import { z as z3 } from 'zod3';
 // NOTE(shafu): this was changed in V2, it does not support network names like base
 const ChainIdSchema = z3.custom<Network>(
   val =>
-    typeof val === 'string' && /^[a-z][a-z0-9-]{2,}:[a-zA-Z0-9._-]+$/.test(val),
+    typeof val === 'string' && /^[-a-z0-9]{3,8}:[-a-zA-Z0-9]{1,64}$/.test(val),
   { message: 'Invalid CAIP-2 network format' }
 );
 
