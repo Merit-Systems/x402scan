5b27bd9e610fefc16f7467931bf26e5227c1c440
Sri Akula <xpaysh@gmail.com>
2026-04-09 08:21:42 -0700

    feat: add Xpay facilitator

--- packages/external/facilitators/src/lists/all.ts
+++ packages/external/facilitators/src/lists/all.ts
@@ -27,6 +27,7 @@ import {
   relaiFacilitator,
   bitrefillFacilitator,
   cascadeFacilitator,
+  xpayFacilitator,
 } from '../facilitators';
 
 import { validateUniqueFacilitators } from './validate';
@@ -62,6 +63,7 @@ const FACILITATORS = validateUniqueFacilitators([
   relaiFacilitator,
   bitrefillFacilitator,
   cascadeFacilitator,
+  xpayFacilitator,
 ]);
 
 export const allFacilitators: Facilitator[] =
