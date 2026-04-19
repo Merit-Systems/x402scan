9b57ceabca68abf60a72eeafdc2aa1c1027af364
Petteri Leinonen <petteri@asterpay.io>
2026-04-19 21:48:55 +0300

    feat: add AsterPay facilitator

--- packages/external/facilitators/src/lists/all.ts
+++ packages/external/facilitators/src/lists/all.ts
@@ -27,6 +27,7 @@ import {
   relaiFacilitator,
   bitrefillFacilitator,
   cascadeFacilitator,
+  asterpayFacilitator,
 } from '../facilitators';
 
 import { validateUniqueFacilitators } from './validate';
@@ -62,6 +63,7 @@ const FACILITATORS = validateUniqueFacilitators([
   relaiFacilitator,
   bitrefillFacilitator,
   cascadeFacilitator,
+  asterpayFacilitator,
 ]);
 
 export const allFacilitators: Facilitator[] =
