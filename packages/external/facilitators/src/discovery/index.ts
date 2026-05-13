cd9baa79d4a5d910ec1f397261a4c3b3977feb62
Mason Hall <masonhall@gmail.com>
2026-05-13 14:39:08 -0400

    fix fluxa

--- packages/external/facilitators/src/discovery/index.ts
+++ packages/external/facilitators/src/discovery/index.ts
@@ -11,5 +11,6 @@ export { questflowDiscovery } from '../facilitators/questflow';
 export { ultravioletadaoDiscovery } from '../facilitators/ultravioletadao';
 export { anyspendDiscovery } from '../facilitators/anyspend';
 export { relaiDiscovery } from '../facilitators/relai';
+export { fluxaDiscovery } from '../facilitators/fluxa';
 
 export { discoverableFacilitators } from './facilitators';
