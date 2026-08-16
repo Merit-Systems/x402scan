a009e081f88d1dd72a378badb77ae8caa6d698a1
alex47 <alexwlex143@gmail.com>
2026-08-16 08:03:41 +0800

    Add botpay facilitator configuration and metadata

---
+++ packages/external/facilitators/botpay.ts
@@ -0,0 +1,33 @@
+import { Network } from '../types';
+import { USDC_BASE_TOKEN } from '../constants';
+
+import type { Facilitator, FacilitatorConfig } from '../types';
+
+export const botpay: FacilitatorConfig = {
+  url: 'https://facilitator.botpay.network',
+};
+
+export const botpayDiscovery: FacilitatorConfig = {
+  url: 'https://facilitator.botpay.network',
+};
+
+export const botpayFacilitator = {
+  id: 'botpay',
+  metadata: {
+    name: 'BotPay',
+    image: 'https://x402scan.com/botpay.png',
+    docsUrl: 'https://botpay.network',
+    color: '#3F96FF',
+  },
+  config: botpay,
+  discoveryConfig: botpayDiscovery,
+  addresses: {
+    [Network.BASE]: [
+      {
+        address: '0x4af9c96576D17BBD37E17814c040ebc73c12C956',
+        tokens: [USDC_BASE_TOKEN],
+        dateOfFirstTransaction: new Date('2026-08-14'),
+      },
+    ],
+  },
+} as const satisfies Facilitator;
