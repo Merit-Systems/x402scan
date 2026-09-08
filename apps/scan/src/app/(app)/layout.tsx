a0493e4804c9cc3305a8f0645c664be632855269
Jason Hedman <40368124+jasonhedman@users.noreply.github.com>
2026-09-08 13:43:29 -0400

    Use the URL as the sole public chain filter state

--- apps/scan/src/app/(app)/layout.tsx
+++ apps/scan/src/app/(app)/layout.tsx
@@ -1,20 +1,13 @@
 import { Footer } from "./_components/layout/footer";
 import { Header } from "./_components/layout/header";
-import { ChainProvider } from "./_contexts/chain/provider";
 import { SearchProvider } from "./_contexts/search/provider";
 
-import { getDataChainCookieServer } from "./_contexts/chain/cookies/server";
-
-export default async function AppLayout({ children }: LayoutProps<"/">) {
-  const initialChain = await getDataChainCookieServer();
-
+export default function AppLayout({ children }: LayoutProps<"/">) {
   return (
-    <ChainProvider initialChain={initialChain}>
-      <SearchProvider>
-        <Header />
-        <div className="flex flex-1 flex-col bg-background">{children}</div>
-        <Footer />
-      </SearchProvider>
-    </ChainProvider>
+    <SearchProvider>
+      <Header />
+      <div className="flex flex-1 flex-col bg-background">{children}</div>
+      <Footer />
+    </SearchProvider>
   );
 }
